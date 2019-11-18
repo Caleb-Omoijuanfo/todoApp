a7a55c4e87e634fb8f71319bcf9890fcf97e2450
caleb Omoijuanfo <c.omoijuanfo@gmail.com>
2019-11-18 23:28:42 +0100

    Updates

---
+++ precache-manifest.ddc6186c63bf1ebde2f395a70fed638e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b992a3fccf976308b0fb3dea6a051832",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "576a61e6f287b03231b4",
+    "url": "/todoApp/static/css/main.d159c4ed.chunk.css"
+  },
+  {
+    "revision": "98df6cb49726a5613790",
+    "url": "/todoApp/static/js/2.e23c20fd.chunk.js"
+  },
+  {
+    "revision": "576a61e6f287b03231b4",
+    "url": "/todoApp/static/js/main.96f77bcb.chunk.js"
+  },
+  {
+    "revision": "7f82fd2bcc96b25c9a23",
+    "url": "/todoApp/static/js/runtime-main.584784af.js"
+  }
+]);
